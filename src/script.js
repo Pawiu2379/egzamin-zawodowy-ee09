66995f5cc06e891a54ed805e598ef11a4a4a5cb8
Pawiu__ <pawelkoc681@gmail.com>
2022-11-09 22:03:49 +0100

    dawdwa

--- src/script.js
+++ src/script.js
@@ -3,28 +3,31 @@ array = document.getElementsByClassName('PrzyciskiDzialania');
 var a = document.getElementById('LiczbaA').value;
 var b = document.getElementById('LiczbaB').value;
 
+var wynik = document.getElementById('wynik')
+
 a = Number(a)
 b = Number(b)
 
 array[0].addEventListener('click',()=>{
-    console.log(a+b)
+    wynik.innerHTML = "Wynik to " +  a+b
 })
 
 array[1].addEventListener('click',()=>{
-    console.log(a-b)
+    wynik.innerHTML = "Wynik to " +  (a-b)
 })
 
 array[2].addEventListener('click',()=>{
-    console.log(a*b)
+    wynik.innerHTML = "Wynik to " +  a*b
 })
 
 array[3].addEventListener('click',()=>{
-    console.log(a/b)
+    wynik.innerHTML = "Wynik to " +  a/b
 })
 
 array[4].addEventListener('click',()=>{
     for(var i = 0; i == b ; i++){
         a = a*a
     }
-    console.log(a)
+    wynik.innerHTML = "Wynik to " +  a
 })
+
