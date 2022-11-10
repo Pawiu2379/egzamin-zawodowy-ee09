7e37eeed792461ddb2953fdb79a9405fb7188fd5
Pawiu__ <pawelkoc681@gmail.com>
2022-11-10 11:21:34 +0100

    the end of work

--- src/script.js
+++ src/script.js
@@ -1,33 +1,41 @@
 var array = []
 array = document.getElementsByClassName('PrzyciskiDzialania');
-var a = document.getElementById('LiczbaA').value;
-var b = document.getElementById('LiczbaB').value;
-
-var wynik = document.getElementById('wynik')
-
-a = Number(a)
-b = Number(b)
+var wynik = document.getElementById('wynik');
+var c = 0;
 
 array[0].addEventListener('click',()=>{
-    wynik.innerHTML = "Wynik to " +  a+b
+    var a = document.getElementById('LiczbaA').value;
+    var b = document.getElementById('LiczbaB').value;
+    var c = Number(a) + Number(b)
+    wynik.innerHTML = `Wynik to ${c}`
 })
 
 array[1].addEventListener('click',()=>{
-    wynik.innerHTML = "Wynik to " +  (a-b)
+    var a = document.getElementById('LiczbaA').value;
+    var b = document.getElementById('LiczbaB').value;
+    var c =a-b
+    wynik.innerHTML = "Wynik to " +  c
+    // console.log(c)
 })
 
 array[2].addEventListener('click',()=>{
-    wynik.innerHTML = "Wynik to " +  a*b
+    var a = document.getElementById('LiczbaA').value;
+    var b = document.getElementById('LiczbaB').value;
+    var c = a*b
+    wynik.innerHTML = `Wynik to ${c}`
 })
 
 array[3].addEventListener('click',()=>{
-    wynik.innerHTML = "Wynik to " +  a/b
+    var a = document.getElementById('LiczbaA').value;
+    var b = document.getElementById('LiczbaB').value;
+    var c = a/b
+    wynik.innerHTML = `Wynik to ${c}`
 })
 
 array[4].addEventListener('click',()=>{
-    for(var i = 0; i == b ; i++){
-        a = a*a
-    }
-    wynik.innerHTML = "Wynik to " +  a
+    var a = document.getElementById('LiczbaA').value;
+    var b = document.getElementById('LiczbaB').value;
+    var c = Math.pow(a,b)
+    wynik.innerHTML = `Wynik to ${c}`
 })
 
